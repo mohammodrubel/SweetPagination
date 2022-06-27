990bb8dcc9ef3f1fd9f891bf72f05b9f4eb01ba9
fardintzbeed@gmail.com <mohammodrubel>
2022-06-28 03:21:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import Sweetpagination from 'sweetpagination';
 
 function App() {
+  const [totaldata,setTotalData] = useState([])
+  const [currentPageData, setCurrentPageData] = useState(new Array(2).fill());
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setTotalData(data))
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          {
+            currentPageData.map(singledata => <h1>{singledata.title}</h1>)
+          }
+        </div>
+
+       <Sweetpagination
+        currentPageData={setCurrentPageData}
+        dataPerPage={5}
+        getData={totaldata}
+      />
+      </div>
   );
 }
 
